7ca79d0dabd66e6b40e242fec59e7f768b673b01
Roderick Gray <rod@Rodericks-MacBook-Air.local>
2026-04-21 11:30:20 -0500

    Initial commit to repo

---
+++ src/main/java/com/example/litness/LitnessApplication.java
@@ -0,0 +1,13 @@
+package com.example.litness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitnessApplication.class, args);
+	}
+
+}
